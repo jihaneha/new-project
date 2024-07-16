2c75b78e5aacbabe19687864a14367182b11705c
jihaneha <ghamidou800@gmail.com>
2024-07-16 12:40:10 +0200

    feat / login button

--- src/theme/feature/layout/auth/LoginButton.tsx
+++ src/theme/feature/layout/auth/LoginButton.tsx
@@ -3,12 +3,19 @@
 import { Button } from "@/components/ui/button";
 import { LogIn } from "lucide-react";
 import { signIn } from "next-auth/react";
+import { useTransition } from "react";
 
 export const LoginButton = () => {
+  const [isPending, startTransition] = useTransition();
   return (
-    <Button size="sm" onClick={() => signIn()}>
+    <Button
+      size="sm"
+      onClick={() => {
+        startTransition(() => signIn());
+      }}
+    >
       <LogIn className="mr-2 h-4 w-4" />
-      Login
+      {isPending ? "loading" : "login"}
     </Button>
   );
 };
